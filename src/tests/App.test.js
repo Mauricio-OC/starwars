c67c9b11752cbd9db57468f072fd3e3528bfa2c5
Mauricio-OC <murisc0@hotmail.com>
2023-01-31 11:17:21 -0300

    feat: cobre mais linhas de testes

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -20,15 +20,18 @@ describe('Testes do projeto Star Wars', () => {
     const filter = screen.getByTestId('column-filter');
     const comparison = screen.getByTestId('comparison-filter');
     const number = screen.getByTestId('value-filter');
+    const btnFilter = screen.getByTestId('button-filter');
     waitFor(() => expect(screen.getAllByRole('row')).toHaveLength(eleven));
     userEvent.selectOptions(filter, ['orbital_period']);
     userEvent.selectOptions(comparison, ['maior que']);
     userEvent.type(number, '1000');
+    userEvent.click(btnFilter);
     userEvent.selectOptions(filter, ['rotation_period']);
     userEvent.selectOptions(comparison, ['maior que']);
     userEvent.selectOptions(comparison, ['igual a']);
     userEvent.selectOptions(comparison, ['menor que']);
     userEvent.type(number, '20');
+    userEvent.click(btnFilter);
 
     const planetBespin = screen.findByRole('cell', { name: /bespin/i });
     expect(planetBespin).toBeDefined();
@@ -41,11 +44,13 @@ describe('Testes do projeto Star Wars', () => {
     await act(async () => {
       renderWithContext(<App />);
     });
+
     expect(global.fetch).toHaveBeenCalled();
     const name = screen.getByTestId(/name-filter/i);
     const column = screen.getByTestId(/column-filter/i);
     expect(name).toBeInTheDocument;
     expect(column).toBeInTheDocument;
+    expect(btnFilter).toBeInTheDocument;
     userEvent.type(name, 'tato');
   });
 });
